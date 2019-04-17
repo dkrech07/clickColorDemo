40a7d70518efb4e04e6db7354060d7e6eebc0e8a
Дмитрий Кречетников <dkrech07@gmail.com>
2019-04-17 14:23:44 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var count = 0;
+
+var colors = ['green', 'red', 'blue', 'yellow', 'pink'];
+
+var elem = document.querySelector('.block');
+
+elem.addEventListener('click', function() {
+  elem.style.background = colors[count];
+
+  if (count === colors.length - 1) {
+    count = 0
+  } else {
+    count = count + 1;
+  };
+});
